5e2b46aa999087c62b74b42556fbfdc857a3ab86
Susana García Rey <susanagarey@gmail.com>
2025-11-16 17:40:42 +0100

    Implementado PanelAuditoria

---
+++ VitalTrack/VitalTrack/ViewModels/UsuarioAuditoriaViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalTrack.ViewModels
+{
+    class UsuarioAuditoriaViewModel
+    {
+        public ulong AuditoriaId { get; set; }
+
+        public string Accion { get; set; } = null!;
+
+        public string? Detalles { get; set; }
+
+        public DateTime CreadoEn { get; set; }
+
+        public uint UsuarioId { get; set; }
+
+        public bool? Activo { get; set; }
+
+        public string? NombreUsuario { get; set; } = null!;
+
+        public string? Nombre { get; set; } = null!;
+
+        public string? Apellidos { get; set; } = null!;
+
+        public string? Foto { get; set; }
+
+        public string NombreRol { get; set; } = null!;
+    }
+}
--- VitalTrack/VitalTrack/Views/PanelAuditoria.xaml.cs
+++ VitalTrack/VitalTrack/Views/PanelAuditoria.xaml.cs
@@ -12,6 +12,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.EntityFrameworkCore;
+using VitalTrack.Data;
+using VitalTrack.Models;
+using VitalTrack.ViewModels;
 
 namespace VitalTrack.Views
 {
@@ -20,9 +24,54 @@ namespace VitalTrack.Views
     /// </summary>
     public partial class PanelAuditoria : UserControl
     {
+        UsuarioAuditoriaViewModel? registroActual;
         public PanelAuditoria()
         {
             InitializeComponent();
+            RefrescarListaRegistrosAuditoria();
+        }
+
+        private void RefrescarListaRegistrosAuditoria()
+        {
+            using var db = new VitaltrackContext();
+
+            var registrosAuditoria = db.Auditoria.
+                Include(a => a.Usuario)
+                .Select(a => new UsuarioAuditoriaViewModel
+                {
+                    AuditoriaId   = a.AuditoriaId,
+                    Accion        = a.Accion,
+                    Detalles      = a.Detalles,
+                    CreadoEn      = a.CreadoEn,
+                    UsuarioId     = a.UsuarioId ?? 0,
+                    Activo        = a.Usuario.Activo ?? false,
+                    NombreUsuario = a.Usuario.NombreUsuario ?? string.Empty,
+                    Nombre        = a.Usuario.Nombre ?? string.Empty,
+                    Apellidos     = a.Usuario.Apellidos ?? string.Empty,
+                    Foto          = a.Usuario.Foto ?? App.FotoPorDefecto,
+                    NombreRol     = db.UsuariosRoles
+                                      .Where(ur => ur.UsuarioId == a.UsuarioId)
+                                      .Join(db.Roles,
+                                            ur => ur.RolId,
+                                            r => r.RolId,
+                                            (ur, r) => r.Nombre)
+                                      .FirstOrDefault() ?? "Sin Rol"
+                })
+                .ToList();
+
+            if (registroActual == null && registrosAuditoria.Count > 0)
+            {
+                registroActual = registrosAuditoria[0];
+            }
+
+            gridRegistros.ItemsSource = registrosAuditoria;
+        }
+
+        private void gridRegistros_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            txtJSON.Text = gridRegistros.SelectedItem is UsuarioAuditoriaViewModel seleccionado
+                ? seleccionado.Detalles ?? string.Empty
+                : string.Empty;
         }
     }
 }
